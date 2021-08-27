67d88b97b019a61617eccba77990985661e7e1d8
Necdet UYGUR <necdet.uygur@gmail.com>
2021-08-27 11:24:46 +0300

    transition

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,9 @@
     <router-link to="/about">Hakkında</router-link>
   </div>
   <hr />
-  <router-view />
+  <transition name="slide" mode="out-in">
+    <router-view />
+  </transition>
 </template>
 
 <script>
@@ -15,3 +17,35 @@ export default {
   name: "App",
 };
 </script>
+
+<style>
+.slide-enter {
+  opacity: 0;
+}
+.slide-enter-active {
+  animation: slide-in 0.25s ease-out forwards;
+  transition: opacity 0.5s;
+}
+.slide-leave-active {
+  animation: slide-out 0.25s ease-out forwards;
+  transition: opacity 0.5s;
+  opacity: 0;
+  position: absolute;
+}
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0px);
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateY(0px);
+  }
+  to {
+    transform: translateY(20px);
+  }
+}
+</style>
